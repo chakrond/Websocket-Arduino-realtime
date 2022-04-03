510709ce0b4516bba2c6760f7afb9a82a728c47e
Chakron <godlooter@gmail.com>
2022-04-03 19:00:16 +0700

    test

--- src/app.js
+++ src/app.js
@@ -1,33 +1,38 @@
-const app = require('express');
-const http = require('http').createServer(app);
-const io = require('socket.io')(http);
+const express = require('express')
+const http = require('http')
+const io = require('socket.io')
+
+const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
+
 
 io.on('connection', (socket) => {
 
-  console.log('Connected');
-  console.log(socket.id);
+  console.log('Connected')
+  console.log(socket.id)
   console.log("JWT token test: ",socket.handshake.headers)
 
   socket.on('event_name', (data) => {
 
-    console.log("Message from Client : ", data);
+    console.log("Message from Client : ", data)
 
-    socket.broadcast.emit("Send Message socket.broadcast.emit : ", data);
-    io.emit("Send Message io.emit Broadcasted : ", data);
-    socket.emit("Send Message : ", data);
+    socket.broadcast.emit("Send Message socket.broadcast.emit : ", data)
+    io.emit("Send Message io.emit Broadcasted : ", data)
+    socket.emit("Send Message : ", data)
 
   })
   
   socket.on('disconnect', () => {
 
-    console.log('Disconnected');
+    console.log('Disconnected')
 
   })
 
 })
 
-http.listen(8080, () => {
+server.listen(8080, () => {
 
-  console.log("Server launched on port 8080");
+  console.log("Server launched on port 8080")
 })
 
