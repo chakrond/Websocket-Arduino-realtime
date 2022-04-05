8af1ed2f55c75c4f2f4044cd5bfb09e83cf437cb
Chakron <godlooter@gmail.com>
2022-04-05 14:12:05 +0700

    upload file

--- src/controllers/home.js
+++ src/controllers/home.js
@@ -1,6 +1,6 @@
 const path = require("path");
 const home = (req, res) => {
-  return res.sendFile(path.join(__dirname, '../templates/views/index.html'))
+  return res.sendFile(path.join(__dirname, '/templates/views/index.html'))
 }
 module.exports = {
   getHome: home
