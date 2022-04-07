555b11d3a3ab47a31c2f7836f3b943388e1428f3
Chakron <godlooter@gmail.com>
2022-04-07 22:54:59 +0700

    upload file

--- src/controllers/upload.js
+++ src/controllers/upload.js
@@ -48,20 +48,6 @@ const getListFiles = async (req, res) => {
       })
     }
 
-    let fileInfos = []
-    await cursor.forEach((doc) => {
-      fileInfos.push({
-        name: doc.filename,
-        url: baseUrl + doc.filename,
-        length: doc.length
-      })
-    })
-
-    console.log(`fileInfos: ${fileInfos}`)
-
-    const fileContLen = fileInfos.length
-    console.log(`fileContLen: ${fileContLen}`)
-
     return res.status(200).send(fileInfos)
 
   } catch (error) {
@@ -84,7 +70,7 @@ const download = async (req, res) => {
     const collection = database.collection(dbConfig.sketchBucket + ".files")
     const cursor = collection.find({ filename: req.params.name })
 
-    let fileInfos = []
+    let fileInfos = {}
     await cursor.forEach((doc) => {
       fileInfos.push({
         name: doc.filename,
