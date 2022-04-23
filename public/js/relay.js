2dc59372647bbc067ffe209756dc40005fd6011e
chakrd <godlooter@gmail.com>
2022-04-23 18:53:46 +0700

    'auto'

--- public/js/relay.js
+++ public/js/relay.js
@@ -34,33 +34,33 @@ swtichRelay1.addEventListener('change', (e) => {
 })
 
 // ------------------------------------------------------------
-// Relay 1
+// Relay 2
 // ------------------------------------------------------------
 
-// get form element
-const swtichRelay2 = document.getElementById('switchCheck-Relay-2')
+// // get form element
+// const swtichRelay2 = document.getElementById('switchCheck-Relay-2')
 
-swtichRelay2.addEventListener('change', (e) => {
+// swtichRelay2.addEventListener('change', (e) => {
 
-    e.preventDefault()
+//     e.preventDefault()
 
-    if (e.target.checked) {
+//     if (e.target.checked) {
 
-        socket.emit('event_relay', {
+//         socket.emit('event_relay', {
 
-            username: "esp8266-5V2R-01",
-            relay2: "true"
-        })
+//             username: "esp8266-5V2R-01",
+//             relay2: "true"
+//         })
 
-    } else {
+//     } else {
 
-        socket.emit('event_relay', {
+//         socket.emit('event_relay', {
 
-            username: "esp8266-5V2R-01",
-            relay2: "false"
-        })
-    }
+//             username: "esp8266-5V2R-01",
+//             relay2: "false"
+//         })
+//     }
 
-    console.log('Relay2 Command was delivered')
+//     console.log('Relay2 Command was delivered')
 
-})
+// })
