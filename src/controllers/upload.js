b30213afa040959552d8824f9a7f8124084e54b4
chakrd <godlooter@gmail.com>
2022-05-07 17:39:08 +0700

    'auto'

--- src/controllers/upload.js
+++ src/controllers/upload.js
@@ -114,6 +114,9 @@ const update = async (req, res) => {
     const collection = database.collection(dbConfig.sketchBucket + '.files')
 
     // Find files
+    if (!req.query.name) {
+      return res.status(200)
+    }
     const regE = new RegExp(`^${req.params.board}-${req.query.name}-OTA-Cloud-ver`) // Filename format board-name-OTA-Cloud-ver1.0
     // const regE = new RegExp(`^${req.params.board}-OTA-Cloud-ver`)
     console.log(`Request Filename: ${regE}`)
--- src/routers/index.js
+++ src/routers/index.js
@@ -1,30 +1,27 @@
-const express = require("express")
+const express = require('express')
 const router = express.Router()
-const homeController = require("../controllers/home")
-const uploadController = require("../controllers/upload")
-const relayController = require("../controllers/relay")
-const boardSettingsController = require("../controllers/control")
+const homeController = require('../controllers/home')
+const uploadController = require('../controllers/upload')
+const relayController = require('../controllers/relay')
+const boardSettingsController = require('../controllers/control')
 
 let routes = app => {
 
   // Upload & Download Skecth
-  router.get("/home", homeController.getHome)
-  router.post("/upload", uploadController.uploadFiles)
-  router.get("/Listfiles", uploadController.getListFiles)
-  router.get("/Listfiles/:name", uploadController.getListSpcFiles)
-  router.get("/update/:board", uploadController.update)
-  router.get("/download/:board/:name", uploadController.download)
+  router.get('/home', homeController.getHome)
+  router.post('/upload', uploadController.uploadFiles)
+  router.get('/Listfiles', uploadController.getListFiles)
+  router.get('/Listfiles/:name', uploadController.getListSpcFiles)
+  router.get('/update/:board', uploadController.update)
+  router.get('/download/:board/:name', uploadController.download)
 
   // Relay control
-  router.get("/relay", relayController.getRelay)
+  router.get('/relay', relayController.getRelay)
 
   // Board control page
-  router.get("/control", boardSettingsController.getControl)
+  router.get('/control', boardSettingsController.getControl)
 
-  // test
-  router.get("/update/:board?name=", homeController.getHome)
-
-  return app.use("/", router)
+  return app.use('/', router)
 }
 
 module.exports = routes
