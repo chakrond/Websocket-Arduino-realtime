cc6dbc037edbf1978b204c886ad352ce45d9e864
chakrd <godlooter@gmail.com>
2022-05-15 11:55:05 +0700

    'auto'

--- public/js/chart.js
+++ public/js/chart.js
@@ -14,54 +14,48 @@ socket.on('joined', (dev) => {
     socket.emit('reqChartData', { ClientId: dev.id, query })
 })
 
-var getChartData = {}
+var config
 socket.on('ChartData', (data) => {
 
     console.log('ChartData: ', data)
-    Object.assign(getChartData, data)
 
-})
+    const labels = data.recTime
 
-const labels = getChartData.recTime
-console.log('getChartData: ', getChartData)
-console.log('recTime: ', getChartData.recTime)
-console.log('DHT21_IN: ', getChartData.DHT21_IN)
-console.log('dsTemp_IN: ', getChartData.dsTemp_IN)
-
-const data = {
-    labels: labels,
-    datasets: [
-        {
-            label: 'DHT21_IN',
-            data: getChartData.DHT21_IN,
-            borderColor: '#4dc9f6',
-            backgroundColor: '#4dc9f6',
-        },
-        {
-            label: 'dsTemp_IN',
-            data: getChartData.dsTemp_IN,
-            borderColor: '#58595b',
-            backgroundColor: '#58595b',
-        }
-    ]
-}
-
-const config = {
-    type: 'line',
-    data: data,
-    options: {
-        responsive: true,
-        plugins: {
-            legend: {
-                position: 'top',
+    const Linedata = {
+        labels: labels,
+        datasets: [
+            {
+                label: 'DHT21_IN',
+                data: data.DHT21_IN,
+                borderColor: '#4dc9f6',
+                backgroundColor: '#4dc9f6',
             },
-            title: {
-                display: true,
-                text: 'Line Chart'
+            {
+                label: 'dsTemp_IN',
+                data: data.dsTemp_IN,
+                borderColor: '#58595b',
+                backgroundColor: '#58595b',
             }
-        }
-    },
-}
+        ]
+    }
+
+    config = {
+        type: 'line',
+        data: Linedata,
+        options: {
+            responsive: true,
+            plugins: {
+                legend: {
+                    position: 'top',
+                },
+                title: {
+                    display: true,
+                    text: 'Line Chart'
+                }
+            }
+        },
+    }
+})
 
 const myChart = new Chart(
     document.getElementById('myChart'),
