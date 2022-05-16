dbdefce701bbb8f56289486a1ec216c861289dc1
chakrd <godlooter@gmail.com>
2022-05-16 18:56:40 +0700

    'auto'

--- public/js/control.js
+++ public/js/control.js
@@ -64,6 +64,16 @@ const btn_save_3 = timerForm.querySelector('button')
 const btn_restart = document.getElementById('btn_restart')
 const btn_update = document.getElementById('btn_update')
 
+// Plot
+const form_chart = document.getElementById('chart_query_form')
+const ipt_chart_query = document.getElementById('chart_query')
+const btn_plot = document.getElementById('btn_plot')
+const rdo_day = document.getElementById('option_day')
+const rdo_range = document.getElementById('option_range')
+const rdo_month = document.getElementById('option_month')
+const rdo_year = document.getElementById('option_year')
+
+
 
 // get device Info
 socket.on('deviceInfo', (dev) => {
@@ -73,7 +83,7 @@ socket.on('deviceInfo', (dev) => {
     console.log('isManualMode: ', dev.mode['isManualMode'])
     if (dev.mode['isManualMode'] == 'true') {
         swtichManual.checked = true
-        const dateTime = new Date (dev.mode['closeTime'])
+        const dateTime = new Date(dev.mode['closeTime'])
         const hr = dateTime.getHours()
         const mm = dateTime.getMinutes()
         const dateString = `AUTO CLOSE AT ${hr}:${mm}`
@@ -176,7 +186,7 @@ settingsForm_1.addEventListener('submit', (e) => {
 
     // Emit event sendMsg to server
     socket.emit('event_control', {
-        
+
         username: devname,
         issetTempChange: 'true',
         ...settings_values_1
@@ -286,13 +296,10 @@ btn_restart.addEventListener('click', () => {
     // disabled button
     btn_restart.setAttribute('disabled', 'disabled')
 
-    socket.emit('event_control', {
+    // enable button
+    btn_restart.removeAttribute('disabled')
 
-        username: devname,
-        restartBoard: 'true',
-    })
-
-    console.log('Command, Restart Board: ', devname)
+    console.log('Command, Plot: ', devname)
 })
 
 btn_update.addEventListener('click', () => {
@@ -306,8 +313,46 @@ btn_update.addEventListener('click', () => {
         updateSketch: 'true',
     })
 
+    // enable button
+    btn_update.removeAttribute('disabled')
+
     console.log('Command, Update Board: ', devname)
 })
+
+
+// ------------------------------------------------------------
+// Plot
+// ------------------------------------------------------------
+form_chart.addEventListener('submit', () => {
+
+    // disabled button
+    btn_plot.setAttribute('disabled', 'disabled')
+
+    // check radio attribute
+    var stringURL
+    if (rdo_day.checked == true) {
+        stringURL = 'by'
+    }
+    if (rdo_range.checked == true) {
+        stringURL = 'range'
+    }
+    if (rdo_month.checked == true) {
+        stringURL = 'month'
+    }
+    if (rdo_year.checked == true) {
+        stringURL = 'year'
+    }
+
+    const inputValue = ipt_chart_query.value
+
+    window.open(`/chart?devname=${devname}&${stringURL}=${inputValue}`)
+
+    // enable button
+    btn_save_3.removeAttribute('disabled')
+
+    console.log('Command, Plot: ', `${stringURL}, ${inputValue}`)
+})
+
 
 
 // ------------------------------------------------------------
