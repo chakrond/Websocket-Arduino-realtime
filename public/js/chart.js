8c01fb5c8ca07fd2888c6a914b29946d4afdfa48
chakrd <godlooter@gmail.com>
2022-05-17 19:47:08 +0700

    'auto'

--- public/js/chart.js
+++ public/js/chart.js
@@ -50,4 +50,35 @@ socket.on('ChartData', (data) => {
         document.getElementById('myChart'),
         config
     )
+
+    console.log('myChart: ', myChart)
 })
+
+
+var inlineCB = document.getElementById('inlineCB')
+
+// creating checkbox element
+var checkbox = document.createElement('input')
+
+// Assigning the attributes
+// to created checkbox
+checkbox.type = 'checkbox'
+checkbox.name = 'name'
+checkbox.value = 'value'
+checkbox.id = 'id'
+
+// creating label for checkbox
+var label = document.createElement('label');
+
+// assigning attributes for 
+// the created label tag 
+label.htmlFor = 'id'
+
+// appending the created text to 
+// the created label tag 
+label.appendChild(document.createTextNode('This is the label for checkbox.'))
+
+// appending the checkbox
+// and label to div
+inlineCB.appendChild(checkbox)
+inlineCB.appendChild(label)
